f3126fdf3d68b84025d6f118f810b353d927ee89
Velvet <saidhage08@gmail.com>
2026-06-18 20:00:31 +0530

    Log deleted message details including executor info

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -50,6 +50,12 @@ export default {
         formatLogLine('Message ID', `\`${message.id}\``),
         formatLogLine('Message author', message.author ? message.author.toString() : 'Unknown'),
         formatLogLine('Message created', `<t:${Math.floor(message.createdTimestamp / 1000)}:R>`),
+        formatLogLine(
+          'Deleted by',
+          deletedBySelf
+            ? `${message.author.toString()} *(self-deleted)*`
+            : `${executor.toString()} \`(${executor.tag})\` 🔨 *moderator action*`
+        ),
       ];
 
       let messageBody = null;
