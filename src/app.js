49cc788dddb85930fdf9451d0324d791977a8997
Velvet <saidhage08@gmail.com>
2026-07-03 12:13:36 +0530

    Import cleanExpiredAFKStatuses from afkService

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ import { initializeMusic } from './services/music/riffySetup.js';
 import { shutdownMusic } from './services/music/playerHandler.js';
 import pkg from '../package.json' with { type: 'json' };
 import { EXPECTED_SCHEMA_VERSION, EXPECTED_SCHEMA_LABEL } from './config/schemaVersion.js';
+import { cleanExpiredAFKStatuses } from './services/afkService.js';
 
 class TitanBot extends Client {
   constructor() {
